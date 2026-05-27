18971b9cec49dbca151903fa1f3448db871f64bf
curry1930 <abhinavakc26@gmail.com>
2026-05-27 16:50:41 +0530

    ignored test and ds_store files

--- summer26/test.py
+++ summer26/test.py
@@ -1,6 +1,2 @@
-num=int(input("enter no btw 1 to 10"))
-
-while num<1 or num>10:
-    print("num is not valid")
-    num=int(input("enter no btw 1 to 10"))
-print(f"{num} is valid!")    
+for i in range(0,10):
+    print(i)
