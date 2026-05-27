2cfccab7a6e9283e53072969db96133df8a88c37
curry1930 <abhinavakc26@gmail.com>
2026-05-27 16:38:56 +0530

    updated code for compound interest calculator

--- summer26/mini projects /compound_int_calculator/compund_int_calculator.py
+++ summer26/mini projects /compound_int_calculator/compund_int_calculator.py
@@ -1,23 +1,55 @@
 #Compound Interest Calculator 
 
-principle =0
-rate =0
-time =0
+#principle =0
+#rate =0
+#time =0
 
-while principle <=0:
+#while principle <=0:
+#    principle=float(input("Enter principle amount: "))
+#    if principle <=0:
+ #       print("Principle amount entered in INVALID!!")
+
+#while rate <=0:
+ #   rate=float(input("Enter interest rate: "))
+  #  if rate <=0:
+#        print("Interest rate entered in INVALID!!")
+
+#while time <=0:
+ #   time=float(input("Enter time(years): "))
+  #  if time <=0:
+   #     print("Interest rate entered in INVALID!!")
+
+#Total=principle * pow((1+rate/100),time)
+#print(f"Total Amount after {time}(years):₹{round(Total,3)}")
+
+
+
+
+
+while True:
     principle=float(input("Enter principle amount: "))
     if principle <=0:
         print("Principle amount entered in INVALID!!")
+    else:
+        break    
 
-while rate <=0:
+while True:
     rate=float(input("Enter interest rate: "))
     if rate <=0:
         print("Interest rate entered in INVALID!!")
+    else:
+        break    
 
-while time <=0:
+while True:
     time=float(input("Enter time(years): "))
     if time <=0:
         print("Interest rate entered in INVALID!!")
+    else:
+        break    
 
 Total=principle * pow((1+rate/100),time)
 print(f"Total Amount after {time}(years):₹{round(Total,3)}")
+
+
+
+
