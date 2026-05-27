f01f9810b362c990e4ed710fb8677a911a7c76e1
curry1930 <abhinavakc26@gmail.com>
2026-05-27 15:09:44 +0530

    added a mini project weight convertor

--- summer26/mini projects /weight_convertor/weight_convertor.py
+++ summer26/mini projects /weight_convertor/weight_convertor.py
@@ -5,4 +5,11 @@ unit=input("Enter unit (lbs/kgs): ")
 
 if unit == "kgs":
     weight *= 2.205
-    print()
+    print(f"Your weight is {round(weight,3)}lbs ")
+elif unit == "lbs":
+    weight /= 2.205
+    print(f"Your weight is {round(weight,3)}kgs ")    
+else:
+    print(f"{unit} entered is INVALID!!")
+
+    
