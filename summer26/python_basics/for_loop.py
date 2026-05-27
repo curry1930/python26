c8cfd5b4d8119e6e3a55d0b7a50923dd2a6aaba9
curry1930 <abhinavakc26@gmail.com>
2026-05-27 17:10:39 +0530

    fixed nested repository issue

---
+++ summer26/python_basics/for_loop.py
@@ -0,0 +1,20 @@
+#for loop = executes a block of code a fixed number of times 
+
+#for i in range(0,11):
+#      print(i)
+
+#reverse of for loop
+#for i in reversed(range(0,11)):
+ #   print(i)
+#print("HAPPY NEW YEAR")    
+
+#stepping
+#for i in range(0,11,2):
+ #   print(i)
+
+#to skip any number
+for i in range(0,21):
+    if i==13:
+        continue
+    print(i)
+    
